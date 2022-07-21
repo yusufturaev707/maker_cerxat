238f49362c77ec12e737c8edbc7786aad123c849
yusufturaev707 <yusufturaev707@gmail.com>
2022-07-21 17:54:56 +0500

    21.07.2022

--- certificate/views.py
+++ certificate/views.py
@@ -4,8 +4,8 @@ from .models import Certificate
 
 # Create your views here.
 def print_cer(request):
-    persons = Certificate.objects.filter(start_date='9').filter(month="iyun")
-    # persons = Certificate.objects.filter(cer_nomer=891)
+    persons = Certificate.objects.filter(start_date='18').filter(month="iyul")
+    # persons = Certificate.objects.filter(cer_nomer=532)
     for p in persons:
         if p.sharf is None:
             p.sharf = ''
